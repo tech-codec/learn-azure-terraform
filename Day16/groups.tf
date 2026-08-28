ee88f1e27a5eb178eaf2b5676249fd11e406656c
tech-codec <varuslimogne@gmail.com>
2026-08-28 02:31:21 -0400

    fea: create new user and  group then add users as  members on ad(EID)

--- Day16/groups.tf
+++ Day16/groups.tf
@@ -33,3 +33,16 @@ resource "azuread_group_member" "engineers" {
   group_object_id  = azuread_group.engineers.object_id
   member_object_id = each.value.object_id
 }
+
+resource "azuread_group" "customer_success" {
+  display_name = "Education - Customer Success"
+  security_enabled = true
+}
+
+resource "azuread_group_member" "customer_success" {
+  for_each = { for u in azuread_user.users: u.mail_nickname => u if u.job_title == "Customer Success" }
+
+  group_object_id = azuread_group.customer_success.object_id
+  member_object_id = each.value.object_id
+}
+
